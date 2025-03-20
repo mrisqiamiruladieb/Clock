cacee75884e3e7970bb321eab09ec16fe37fe931
adieb <mrizqiamiruladieb@gmail.com>
2025-03-20 14:03:14 +0700

    feat: define the time format

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -1,15 +1,32 @@
 import java.awt.*;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import javax.swing.*;
 
 public class MyFrame extends JFrame {
     
+    //declare
+    Calendar calendar;
+    SimpleDateFormat timeFormat;
+    JLabel timeLabel;
+    String time;
+    
     //construct
-    MyFrame() {
+    MyFrame() {    
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("My Clock Program");
         this.setLayout(new FlowLayout());
         this.setSize(350, 200);
         this.setResizable(false);
+        
+        timeFormat = new SimpleDateFormat("HH:mm:ss");
+        
+        timeLabel = new JLabel();
+        
+        time = timeFormat.format(Calendar.getInstance().getTime());
+        timeLabel.setText(time);
+        
+        this.add(timeLabel);
         this.setVisible(true);
     }
     
