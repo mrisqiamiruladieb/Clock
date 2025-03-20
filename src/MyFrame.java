45f54f8258d8e3b1a390b0323561bca183b69007
adieb <mrizqiamiruladieb@gmail.com>
2025-03-20 13:36:56 +0700

    feat: define the frame

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -1,4 +1,16 @@
+import java.awt.*;
+import javax.swing.*;
 
-public class MyFrame {
+public class MyFrame extends JFrame {
+    
+    //construct
+    MyFrame() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("My Clock Program");
+        this.setLayout(new FlowLayout());
+        this.setSize(350, 200);
+        this.setResizable(false);
+        this.setVisible(true);
+    }
     
 }
